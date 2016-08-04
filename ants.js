91d20a0c22185b9584f1f873e05602fd55497b8a
Alejandro del Rio <adelriosantiago@gmail.com>
2016-08-04 00:08:01 -0500

    Add statuses

--- ants.js
+++ ants.js
@@ -3,52 +3,17 @@
     var canvas,
         context,
         imageData,
-        startingAnts = 100,
-        maxAnts = 500,
+        startingAnts = 1000,
+        maxAnts = 1000,
         ants = [],
         defaultAnt = {
             "x" : 10,
             "y" : 10,
             "life" : 255,
-            "waitFor" : function() {
-                return (Math.floor(Math.random() * 1000));
-            },
-            "move" : function() {
-                function selfPaint(x, y, inverse) {
-                    if (inverse) {
-                        imageData.data[0] = 255;
-                        imageData.data[1] = 255;
-                        imageData.data[2] = 255;
-                    } else {
-                        imageData.data[0] = 0;
-                        imageData.data[1] = 0;
-                        imageData.data[2] = 0;
-                    }
-                    imageData.data[3] = 255;
-                    context.putImageData(imageData, x, y);
-                    
-                    return;
-                }
-                function randMove() {
-                    return (Math.floor(Math.random() * 3) - 1);
-                }
-                selfPaint(this.x, this.y, true);
-                this.x += randMove();
-                this.y += randMove();
-                selfPaint(this.x, this.y, false);
-                
-                setTimeout(function() {
-                    this.move();
-                    //console.log("ok");
-                }.bind(this), this.waitFor());
-                
-                return;
-            }
+            "status" : 0, //0=searching for food, 1=food found and returning, 2=over arousal over danger
+            
         };
-        
-        //var test = _.extend(defaultAnt, {"a":1});
-        //console.log(test);
-        
+    
     function init() {
         var i;
 
@@ -56,30 +21,21 @@
         context = canvas.getContext('2d');
         imageData = context.createImageData(1, 1);
         
-        drawPixel(5, 5, 0, 0, 0, 255);
-        drawPixel(5, 6, 0, 0, 0, 255);
-        drawPixel(5, 7, 255, 0, 0, 255);
-        
         for (i = 0; i < startingAnts; i++) {
-            ants.push(_.extend(_.clone(defaultAnt), {"x" : (Math.floor(Math.random() * 200)), "y" : (Math.floor(Math.random() * 200))}));
-            ants[i].move();
+            ants.push(_.extend(_.clone(defaultAnt), {
+                "x" : (Math.floor(Math.random() * 200)),
+                "y" : (Math.floor(Math.random() * 200))
+            }));
         }
         console.log(ants);
-        
-        //ants[0].selfPaint();
+        step(); //Start the engine
     }
 
     function moveAnt(ant) {
         function selfPaint(x, y, inverse) {
-            if (inverse) {
-                imageData.data[0] = 255;
-                imageData.data[1] = 255;
-                imageData.data[2] = 255;
-            } else {
-                imageData.data[0] = 0;
-                imageData.data[1] = 0;
-                imageData.data[2] = 0;
-            }
+            imageData.data[0] = 255 * inverse;
+            imageData.data[1] = 255 * inverse;
+            imageData.data[2] = 255 * inverse;
             imageData.data[3] = 255;
             context.putImageData(imageData, x, y);
             
@@ -101,16 +57,8 @@
         setTimeout(function() {
             step();
             //console.log("ok");
-        }, 5);
+        }, 10);
     }
-
-    function drawPixel(x, y, r, g, b, a) {
-        imageData.data[0] = r;
-        imageData.data[1] = g;
-        imageData.data[2] = b;
-        imageData.data[3] = a;
-        context.putImageData(imageData, x, y);
-    }
-
+    
     init();
 /*})();*/
