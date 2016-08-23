4786529d8bab0f33575a774cd01fcc75e70085cf
Alejandro del Rio <adelriosantiago@gmail.com>
2016-08-22 23:00:22 -0500

    Using canvas as pheromones map instead Colors reestructure

--- ants.js
+++ ants.js
@@ -4,6 +4,19 @@
         //maxAnts = 5000, //Not used temporally
         width = 300,
         height = 300,
+        colors = {
+			empty: [255, 255, 255],
+			ant: {
+				normal: [0, 0, 0],
+				inDanger: [255, 0, 0],
+				dead: [192, 192, 192]
+			},
+			food: [0, 255, 0],
+			trail: {
+				weak: [153, 204, 255],
+				strong: [255, 255, 0]
+			}
+		},
         context,
         imageData,
         canvas,
@@ -11,14 +24,12 @@
         ants = [],
         foods = [],
         defaultAnt = {
-            "id" : null,
             "x" : 10,
             "y" : 10,
             "life" : 255,
-            "overArousal" : true, //True for an over aroused ant
-            "foodFound" : false, //True for an ant that found and carries food
-            "exploring" : true, //True for an ant that is actively looking for food
-            "returning" : false/*, //True for an ant that is returning home
+            "inDanger" : true, //True for an over aroused ant
+            "foodFound" : false, //True for an ant that has found food and is returning
+            "exploring" : true/*, //True for an ant that is actively looking for food
             "moveNext" : false, //True for an ant that is about to move on the next step
             "step" : function() {
                 this.moveNext = true;
@@ -38,7 +49,6 @@
         //Create starting ants
         _.times(startingAnts, function(i) {
             ants.push(_.extend(_.clone(defaultAnt), {
-                "id" : _.sampleSize("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", 10).join(""),
                 "x" : _.random(0, width),
                 "y" : _.random(0, height)
             }));
@@ -51,8 +61,6 @@
                 console.log(x + ", " + y);
             });
         });*/
-        
-        
         //console.log(ants);
         
         //Create starting food sources
@@ -62,7 +70,6 @@
                 
             _.times(_.random(10, 30), function(i) {
                 foods.push({
-                    owner: null,
                     "x": xCenter + _.random(0, 10),
                     "y": yCenter + _.random(0, 10)
                 });
@@ -73,7 +80,7 @@
     }
 
     processWorld = function() {
-        function selfPaint(x, y, color) {
+        function paint(x, y, color) {
             imageData.data[0] = color[0];
             imageData.data[1] = color[1];
             imageData.data[2] = color[2];
@@ -84,24 +91,11 @@
         }
 
         _.each(foods, function(food) {
-            if (food.owner != null) {
-                selfPaint(food.x + 1, food.y, [255, 255, 255]);
-                selfPaint(food.x - 1, food.y, [255, 255, 255]);
-                selfPaint(food.x, food.y + 1, [255, 255, 255]);
-                selfPaint(food.x, food.y - 1, [255, 255, 255]);
-                food.x = _.find(ants, _.matchesProperty('id', food.owner)).x;
-                food.y = _.find(ants, _.matchesProperty('id', food.owner)).y;
-                selfPaint(food.x + 1, food.y, [0, 255, 0]);
-                selfPaint(food.x - 1, food.y, [0, 255, 0]);
-                selfPaint(food.x, food.y + 1, [0, 255, 0]);
-                selfPaint(food.x, food.y - 1, [0, 255, 0]);
-            } else {
-                selfPaint(food.x, food.y, [255, 0, 0]);
-            }
+			paint(food.x, food.y, colors.food);
         })
         
         _.each(ants, function(ant) {
-            selfPaint(ant.x, ant.y, [255, 255, 255]);
+            //selfPaint(ant.x, ant.y, colors.empty);
             
             ant.x += _.random(-1, 1); //Move X
             ant.x = _.clamp(ant.x, width);
@@ -113,18 +107,15 @@
                     if (Math.abs(a - b) < 2) return true;
                     return false;
                 }
-                if ((insideRange(food.x, ant.x)) && (insideRange(food.y, ant.y)) && (food.owner == null) && (ant.foodFound == false)) {
-                    //console.log("TEST");
+                if ((insideRange(food.x, ant.x)) && (insideRange(food.y, ant.y)) && (ant.foodFound == false)) {
                     ant.foodFound = true;
                     ant.returning = true;
                     ant.exploring = false;
-                    //ant.inventory.push(food);
-                    food.owner = ant.id;
                     console.log(food);
                 }
             });
             
-            selfPaint(ant.x, ant.y, [0, 0, 0]);
+            paint(ant.x, ant.y, colors.ant.normal);
         });
         
         setTimeout(function() {
